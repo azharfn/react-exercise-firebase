e6446b37dad818f6bce3118134d890c36b7e2b5c
azharfn <azharfzn12@gmail.com>
2023-06-25 02:44:29 +0700

    Publish Program

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://react-exercise-firebase-bd612.web.app" // TODO: replace this
+const studentName = "Azhar Fauzan Nugroho" // TODO: replace this
+const studentId = "FE4249027" // TODO: replace this
+
+export { url, studentName, studentId }
